70602dbacad45e774364ec9d11377266511ffccb
d-virusss <jyg0599@gmail.com>
2020-07-06 15:23:52 +0000

    CR complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,40 @@
 class HomeController < ApplicationController
 	def index
+		@posts = Post.all
 	end
 	
-	def hi
-		@message = "도망쳐~!"
+	def write
+		
 	end
+	
+	def create
+		post = Post.new
+		post.title = params[:title]
+		post.content = params[:content]
+		post.save
+		
+		redirect_to '/index'
+	end
+	
+	def update
+		post = Post.find(params[:id])
+		post.title = params[:title]
+		post.content = params[:content]
+	end
+	
+	def modify
+		post = Post.find(params[:id])
+		post.title = params[:title]
+		post.content = params[:content]
+		post.save
+		
+		redirect_to '/index'
+	end
+	
+	def delete
+		Post.destroy(params[:id])
+		redirect_to '/index'
+	end
+	
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # root에서는 HomeController의 index액션에 연결해라
-	get '/' => 'home#index'
-	get '/home' => 'home#hi'
+	get '/index' => 'home#index'
+	get '/write' => 'home#write'
+	post '/create' => 'home#create'
+	post '/update/:id' => 'home#update'
+	post '/modify/:id' => 'home#modify'
+	post '/delete/:id' => 'home#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
